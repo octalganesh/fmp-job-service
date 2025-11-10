c4f04f6c8e2341a18a0df581061c09fa7bea70b1
arvindsingh <arvind.singh@octalsoftware.com>
2025-11-10 16:40:33 +0530

    cahgne fo rtenant

--- src/main/java/com/octal/fsm/service/impl/QuickBooksTokenStore.java
+++ src/main/java/com/octal/fsm/service/impl/QuickBooksTokenStore.java
@@ -42,7 +42,7 @@ public class QuickBooksTokenStore {
 
             token.setAccessToken(response.getAccessToken());
             token.setRefreshToken(response.getRefreshToken());
-            token.setExpiresAt(LocalDateTime.now().plusSeconds(response.getExpiresIn()));
+            token.setExpiresAt(LocalDateTime.now().plusMinutes(response.getExpiresIn()));
             token=tokenRepository.save(token);
         }
         return token;
