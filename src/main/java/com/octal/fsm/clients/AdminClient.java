320d73a1dab3fc85438cede8d9e7879d93391992
HardikOctal <hardik.khurana@octalsoftware.com>
2025-11-20 12:23:15 +0530

    Update customer profile API endpoint for technician access

--- src/main/java/com/octal/fsm/clients/AdminClient.java
+++ src/main/java/com/octal/fsm/clients/AdminClient.java
@@ -14,8 +14,8 @@ import static com.octal.fsm.common.CommonConstants.USER_NAME;
 @FeignClient(name = "admin-service")
 public interface AdminClient {
 
-    @GetMapping(value = "/customer-profile/get/by/{id}")
-    ResponseEntity<ApiResponse> getCustomerById(@PathVariable("id") String id, @RequestHeader("userName") String userName);
+    @GetMapping(value = "/customer-profile/get/by-for-technician/{id}")
+    ResponseEntity<ApiResponse> getCustomerById(@PathVariable("id") String id,@RequestHeader("userName") String userName);
 
     @GetMapping(value = "/customer-profile/job-details-lead-customer-details")
     ResponseEntity<ApiResponse> getJobDetailsWithLeadAndCustomerDetails(@RequestParam("customerId") String customerId, @RequestParam("leadSourceId") String leadSourceId, @RequestHeader(USER_NAME) String userName, @RequestHeader("tenantId") Long tenantId,
