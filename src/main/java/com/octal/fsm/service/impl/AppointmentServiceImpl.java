82578d49e56166a4cc56b12535dae8921df3ed16
ganeshoctal <ganesh.prajapati@octalsoftware.com>
2026-01-21 16:43:49 +0530

    changes

--- src/main/java/com/octal/fsm/service/impl/AppointmentServiceImpl.java
+++ src/main/java/com/octal/fsm/service/impl/AppointmentServiceImpl.java
@@ -428,9 +428,9 @@ public class AppointmentServiceImpl implements AppointmentService {
             builder.with(appointmentSpecificationFactory.isLessThanOrEquals("createdAt", listRequest.getEndDate().atTime(23, 59, 59)));
         }
         if (listRequest.getSearchText() != null && !listRequest.getSearchText().isEmpty()) {
-            builder.with(appointmentSpecificationFactory.group(appointmentSpecificationFactory.like("taskId", listRequest.getSearchText()).
+            builder.with(appointmentSpecificationFactory.group(appointmentSpecificationFactory.like("jobTaskId", listRequest.getSearchText()).
                     or(appointmentSpecificationFactory.like("jobId", listRequest.getSearchText())).
-                    or(appointmentSpecificationFactory.isEqual("taskId", listRequest.getSearchText())).
+                    or(appointmentSpecificationFactory.isEqual("jobTaskId", listRequest.getSearchText())).
                     or(appointmentSpecificationFactory.isEqual("jobId", listRequest.getSearchText()))));
         }
 
